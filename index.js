e0dc5fb153273bad006ec1bdf79758914a15211e
Rolf Küffner <rolfkueffner@t-online.de>
2017-03-23 12:02:39 +0100

    rock paper scissors

--- index.js
+++ index.js
@@ -55,54 +55,55 @@ var Module = function (bot) {
 
   this.commands.rps = function(channel, args, user) {
 
-    choice = ["Stein", "Spock", "Papier", "Echse", "Schere"];
+    choice = ["Rock", "Spock", "Paper", "Lizard", "Scissors"];
 
-    art = [["", "Mein Spock verdampft deinen Stein", "Mein Papier bedeckt deinen Stein", "Dein Stein zerquetscht meine Echse", "Dein Stein schleift meine Schere"],
-      ["Dein Spock verdampft meinen Stein", "", "Mein Papier widerlegt deinen Spock", "Meine Echse vergiftet deinen Spock", "Dein Spock zertrümmert meine Schere"],
-      ["Dein Papier bedeckt meinen Stein", "Dein Papier widerlegt meinen Spock", "", "Meine Echse frisst dein Papier", "Meine Schere schneidet dein Papier"],
-      ["Mein Stein zerquetscht deine Echse", "Deine Echse vergiftet meinen Spock", "Deine Echse frisst mein Papier", "", "Meine Schere köpft deine Echse"],
-      ["Mein Stein schleift deine Schere", "Mein Spock zertrümmert deine Schere", "Deine Schere schneidet mein Papier", "Deine Schere köpft meine Echse", ""]];
+    art = [["", "My Spock vaporizes your Rock", "My Paper covers your Rock", "Your Rock crushes my Lizard", "Your rock crushes my Scissors"],
+      ["Your Spock vaporizes my Rock", "", "My Paper disproves Spock", "My Lizard poisons your Spock", "Your Spock smashes my Scissors"],
+      ["Your Paper covers my Rock", "Your Paper disproves my Spock", "", "My Lizard eats your Paper", "My Scissors cuts your Paper"],
+      ["My Rock crushes your Lizard", "Your Lizard poisons my Spock", "Your Lizard eats my Paper", "", "My Scissors decapitates your Lizard"],
+      ["My Rock crushes your Scissors", "My Spock smashes your Scissors", "Your Scissors cuts my Paper", "Your Scissors decapitates my Lizard", ""]];
     var choiceN;
 
     switch (args.toUpperCase()) {
-      case "STEIN":
+      case "STEIN" || "ROCK":
         choiceN = 0;
         break;
       case "SPOCK":
         choiceN = 1;
         break;
-      case "PAPIER":
+      case "PAPIER" || "PAPER":
         choiceN = 2;
         break;
-      case "ECHSE":
+      case "ECHSE" || "LIZARD":
         choiceN = 3;
         break;
-      case "SCHERE":
+      case "SCHERE" || "SCISSORS":
         choiceN = 4;
         break;
       default:
         choiceN = -1;
-        bot.postMessage(data.channel, "Das Spiel heisst SCHERE, STEIN, PAPIER, SPOCK, ECHSE OHNE " + choice + ".");
+        bot.postMessage(channel, "The games called: Rock, Paper, Scissors, Lizard, Spock" );
         break;
     }
 
     if (choiceN != -1){
+      compChoice = Math.floor(Math.random() * 5);
       var diff = choiceN - compChoice;
       var test = (((diff%5)+5)%5);
 
       if (diff === 0) {
-        bot.postMessage(data.channel, "UNENTSCHIEDEN: Wir haben beide "+choice[compChoice]+" gewählt!");
+        bot.postMessage(channel, "TIE: We both chose "+choice[compChoice]+"!");
       }
       else if (test === 3 ||test === 4){
-        bot.postMessage(data.channel, "VERLOREN: "+art[userChoice][compChoice]);
+        bot.postMessage(channel, "YOU LOST: "+art[choiceN][compChoice]);
 
       }
       else if (test === 1 || test === 2){
-        bot.postMessage(data.channel, "GEWONNEN: "+art[userChoice][compChoice]);
+        bot.postMessage(channel, "YOU WON: "+art[choiceN][compChoice]);
       }
     }
   };
-  compChoice = Math.floor(Math.random() * 5);
+
 
 };
 
